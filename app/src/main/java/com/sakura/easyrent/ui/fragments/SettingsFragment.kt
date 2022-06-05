9aaa7f644ab1e78c9c311dcf428acf78b7c6375c
ABDO-AR <arcompany000@gmail.com>
2022-06-05 23:57:20 +0200

    SettingsFragment.kt

--- app/src/main/java/com/sakura/easyrent/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/sakura/easyrent/ui/fragments/SettingsFragment.kt
@@ -5,23 +5,97 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import com.google.gson.Gson
 import com.sakura.easyrent.R
+import com.sakura.easyrent.control.managers.SPManager
+import com.sakura.easyrent.databinding.FragmentSettingsBinding
+import com.sakura.easyrent.model.User
+import com.sakura.easyrent.ui.activitys.main.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import kotlin.system.exitProcess
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+    // Fields:
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding: FragmentSettingsBinding get() = _binding!!
+    @Suppress("unused") private val model: MainViewModel by viewModels()
+
+    // Fields(Managers):
+    @Inject lateinit var manager: SPManager
+
+    // Fields(Strings):
+    private lateinit var userJsonMetaData: String
+
+    // Companion:
+    companion object {
+        // Tags:
+        @Suppress("unused")
+        const val TAG: String = "SettingsFragment"
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {}
+    // Method(OnCreateView):
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        // Initializing:
+        _binding = FragmentSettingsBinding.inflate(layoutInflater, container, false)
+        // Returning:
+        return binding.root
+    }
+
+    // Method(OnViewCreated):
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) { // Here we will do our work.
+        // Super:
+        super.onViewCreated(view, savedInstanceState)
+        // Initializing(Application):
+        initializeUIWithUserMetaData()
+        initializeProgrammableViews()
+    }
+
+    // Method(InitializeUIWithUserMetaData):
+    private fun initializeUIWithUserMetaData() {
+        // Initializing:
+        userJsonMetaData = (manager.read(SPManager.USER_META_DATA, "") as String)
+        // Checking:
+        if (userJsonMetaData.isNotBlank()) {
+            // Initializing:
+            val user = Gson().fromJson(userJsonMetaData, User::class.java)
+            // Applying:
+            binding.apply {
+                // Setting:
+                userNameTextView.text = user.userName
+                editNameOnCard.text = user.email
             }
+        }
+    }
+
+    // Method(InitializeProgrammableViews):
+    private fun initializeProgrammableViews() {
+        // Developing:
+        binding.LogOut.setOnClickListener {
+            // Checking:
+            if (userJsonMetaData.isNotBlank()) {
+                // LoggingOut:
+                manager.write(SPManager.USER_META_DATA, "")
+                manager.write(SPManager.ACCESS_TOKEN, "")
+                manager.write(SPManager.REFRESH_TOKEN, "")
+                // Toasting:
+                Toast.makeText(requireContext(), getString(R.string.logged_out_successfully_msg), Toast.LENGTH_SHORT).show()
+                // Closing:
+                requireActivity().finish()
+                exitProcess(0)
+            }
+        }
+    }
+
+    // Method(OnDestroyView):
+    override fun onDestroyView() { // This will destroy our binding after destroying all views.
+        // Super:
+        super.onDestroyView()
+        // Destroying:
+        _binding = null
     }
 }
