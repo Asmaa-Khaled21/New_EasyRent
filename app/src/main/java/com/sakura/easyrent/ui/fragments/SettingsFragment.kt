30a85703bf1f86a5e575ec0430fd845e141dc108
Asmaa-Khaled21 <93935870+Asmaa-Khaled21@users.noreply.github.com>
2022-08-19 02:08:28 +0200

    Mo

--- app/src/main/java/com/sakura/easyrent/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/sakura/easyrent/ui/fragments/SettingsFragment.kt
@@ -1,11 +1,11 @@
 package com.sakura.easyrent.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.gson.Gson
 import com.sakura.easyrent.R
@@ -66,7 +66,7 @@ class SettingsFragment : Fragment() {
             // Applying:
             binding.apply {
                 // Setting:
-                userNameTextView.text = user.userName
+
                 editNameOnCard.text = user.email
             }
         }
